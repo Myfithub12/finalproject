82e8b02a34b5af37f5c2e95b91e11645cc4cdafb
Mark <mhollrah0001@gmail.com>
2024-08-08 08:52:53 -0400

    08\08\2024

---
+++ script/node.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.array('files'), (req, res) => {
+  res.json({ message: 'Files uploaded successfully!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
